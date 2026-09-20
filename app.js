02f912e7f635d65bee426f7d5a04c3014c066d16
Sanchit Rai <130173661+Sanchit81@users.noreply.github.com>
2026-09-20 17:50:10 +0530

    JSX and Components

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
-/* 
-<div id="parent">
-  <div id=child>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//JSX --> HTML LIKE SYNTAX(!HTML)
+const heading1 = <h1 id="heading1">hello world through JSX </h1>;
+
+const heading2 = (
+  <h1 className="Head2" tabIndex="5">
+    Hello everyone👀!!
+  </h1>
+);
+
+// React components (same as normal js function) --> return react elements.
+// Component composition -> one component can be used inside another component.
+// Always name components in capital.
+// There are many ways/syntax to write a component as shown below.
+
+const Component1 = () => <h2>Hey there!!</h2>;
+
+const heading3 = (
+  <div>
+    <Component1/>
+    <h1>
+    Hello everyone to this new world of trying and learning
+  </h1>
   </div>
-  <div id=child2>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-</div>
-*/
+);
 
-const parent2 = React.createElement('div', { id: 'parent2' }, [
-  React.createElement('div', { id: 'child2_1' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
-    React.createElement('h2', {}, "I'm h2 Tag"),
-  ]),
-  React.createElement('div', { id: 'child2_2' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
-    React.createElement('h2', {}, "I'm h2 Tag"),
-  ]),
-]);
+const Component2 = () => (
+  <>
+    {heading1}
+    <Component1 />
+    <h1 id="Component2">Hello from component2</h1>
+  </>
+);
 
+const Component3 = () => {
+  return <Component2 />; // or const Component3 = () => <Component2 />;
+};
 
-console.log(parent2);
-const root = ReactDOM.createRoot(document.getElementById('root2')); 
-root.render(parent2);           //ReactElement(Object) => HTML(Browser Understands)
+const Component4 = function () {
+  return (
+    <>
+      <Component3 />
+      <h1>Hello from component 4</h1>
+    </>
+  );
+};
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {heading1}
+    {heading2}
+    <Component1 />
+    <Component2 />
+    <Component3 />
+    <Component4 />
+    {heading3}
+  </>,
+);
